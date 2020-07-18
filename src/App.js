994e7a56e21ae875d7a71aea4dcdb9277e0b0e70
siucng2222 <siucng2222-c@my.cityu.edu.hk>
2020-07-18 16:57:06 +0800

    Separate out reducer function

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import CartContainer from "./components/CartContainer";
 import cartItems from "./cart-items";
 // action types
 import { DECREASE, INCREASE } from "./actions";
+// reducer
+import reducer from "./reducer";
 
 // redux stuff
 
@@ -13,54 +15,12 @@ import { DECREASE, INCREASE } from "./actions";
 
 // store - stores data, think of state
 // reducer - function that used to update store
-
 import { createStore } from "redux";
 
 // initial store
 const initialStore = {
   count: 0,
   name: "John Doe",
-};
-
-// reducer
-// two arguments - state, action
-// state = old state/state before update
-// action = what happened / what update
-// return updated  (or old) state
-let reducer = (state, action) => {
-  console.log("in reducer");
-  console.log({ state, action });
-
-  switch (action.type) {
-    case DECREASE:
-      console.log("Decrease Action triggered");
-
-      // DON't do this - State should be IMMUTABLE
-      // state.count = state.count - 1;
-
-      // Make copy with updated state instead
-      // return { count: state.count - 1 };
-
-      // If initialStore return multiple attributes, the above return object which have one attribute only will ruin the state
-      // So should copy the old object before update new value
-      return { ...state, count: state.count - 1 };
-    case INCREASE:
-      console.log("Increase Action triggered");
-      return { ...state, count: state.count + 1 };
-    // case RESET:
-    //   console.log("RESET Action triggered");
-    //   return { ...state, count: 0 };
-    // case CHANGE_NAME:
-    //   console.log("CHANGE_NAME Action triggered");
-    //   return { ...state, name: "Bob" };
-    default:
-      // ALWAYS return state object (either updated or old) from reducer to ensure consistency
-      console.log("Default action");
-      return state;
-
-    // Test return string when createStore, then return object after action
-    // return "String instead of state object";
-  }
 };
 
 console.log("createStore");
