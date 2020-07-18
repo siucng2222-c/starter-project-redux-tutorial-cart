6c7f2c972f47fd740ebd6e06a3b25057cc43de69
siucng2222 <siucng2222-c@my.cityu.edu.hk>
2020-07-18 15:53:51 +0800

    initialize Redux store. Basic Reducer, pass store to React component as prop

--- src/App.js
+++ src/App.js
@@ -6,12 +6,38 @@ import CartContainer from "./components/CartContainer";
 import cartItems from "./cart-items";
 // redux stuff
 
+// UI > Action > Reducer > Store > UI...
+
+// store - stores data, think of state
+// reducer - function that used to update store
+
+import { createStore } from "redux";
+
+// initial store
+const initialStore = { count: 0 };
+
+// reducer
+// two arguments - state, action
+// state = old state/state before update
+// action = what happened / what update
+// return updated  (or old) state
+let reducer = (state, action) => {
+  console.log({ state, action });
+
+  // ALWAYS return state (either updated or old) from reducer
+  return state;
+};
+const store = createStore(reducer, initialStore);
+// Redux store can be passed to React component
+console.log(store.getState());
+
 function App() {
   // cart setup
 
   return (
     <main>
-      <Navbar />
+      {/* Pass Redux store to react component as prop */}
+      <Navbar cart={store.getState()} />
       <CartContainer cart={cartItems} />
     </main>
   );
